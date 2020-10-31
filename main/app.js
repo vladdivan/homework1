503fc10187829b541062c426fb40de0d45448ab1
vladdivan <ivanvlad18@stud.ase.ro>
2020-10-31 14:14:24 +0200

    work done

--- main/app.js
+++ main/app.js
@@ -1,5 +1,36 @@
 function removeOrderItem(orderInfo, position){
+    if(!Array.isArray(orderInfo.items)) {
+        throw Error('Items should be an array');
+    }
+
+    let checkItems
+    for(let i =0; i < orderInfo.items.length; i++){
+        checkItems = true;
+
+        if(orderInfo.items[i].price == undefined){
+            checkItems = false;
+        }
+
+        if(orderInfo.items[i].quantity == undefined){
+            checkItems = false;
+        }
+
+        if(!checkItems)
+        {
+            throw Error('Malformed item');
+        }
+    }
+
+    if( position < 0 || position > orderInfo.items.length - 1){
+        throw Error('Invalid position');
+    }
+
     
+    orderInfo.total = orderInfo.total - (orderInfo.items[position].price * orderInfo.items[position].quantity);
+
+    orderInfo.items.splice(position, 1);
+
+    return orderInfo;
 }
 
 const app = {
